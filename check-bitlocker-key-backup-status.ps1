e9e717d7f659d5b591077df5020d78d2b3b3bfcb
Ben Peters <bpeters@emich.edu>
2017-05-22 09:37:25 -0400

    Added remote desktop script

---
+++ check-bitlocker-key-backup-status.ps1
@@ -0,0 +1,150 @@
+﻿###########################
+# Author: Ben Peters (bpeters@emich.edu)
+#
+# This script will double check a group in active directory for all computer accounts in there, and will report any 
+# computers that do not have keys stored in Active Directory or the MBAM database.
+###########################
+
+# Basic Configs
+$ADComputerGroup = 'some_group_name'
+$SQLServer = "mbamdb.server.example.com"
+$SQLDBName = "MBAM Recovery and Hardware"
+$path = "DC=something,DC=somethingelse,DC=com"
+
+Import-Module ActiveDirectory
+
+$TestOutput = "==================================`n"
+$TestOutput = $TestOutput + "Individual Test Output`n"
+$TestOutput = $TestOutput + "==================================`n`n"
+$ComputersWithNoADKey = ""
+$ComputersWithNoMBAMKey = ""
+$NumberOfComputers = 0
+$NumberMissingMBAMKeys = 0
+$NumberMissingADKeys = 0
+
+# get all the members of the bitlocker fixed drives group
+$Members = Get-ADGroupMember -identity "$ADComputerGroup" | where {$_.objectclass-eq “computer”} | Select-Object -Property name
+
+
+# Go through each computer in the group
+foreach ($Name in $Members) {
+    
+    # Increment counter
+    $NumberOfComputers++
+
+    # Get details about this computer
+    $Data = Get-ADComputer $Name.name -Properties distinguishedName,name,lastLogon,location,operatingSystem,operatingSystemServicePack,operatingSystemVersion
+    
+    # Format some output
+    $TestOutput = $TestOutput + $Name.name + "`n"
+    $TestOutput = $TestOutput +  "-------------------------" + "`n"
+    $TestOutput = $TestOutput + "     " + $Data.operatingSystem + "/" + $Data.operatingSystemServicePack + "/" + $Data.operatingSystemVersion + "`n"
+    
+    #####################
+    # See if it has keys stored in AD or not.
+    #####################
+    $path = $Data.distinguishedName
+    $children = Get-Childitem -Path AD:\$path | where {$_.objectclass-eq “msFVE-RecoveryInformation”}
+
+    # If keys are found, great.  If not, mention that.
+    if (!$children) {
+        $TestOutput = $TestOutput +   "     ***No AD key found!***" + "`n"
+        $ComputersWithNoADKey = $ComputersWithNoADKey + $Name.name+ " : " + [datetime]$Data.lastLogon + " : " + $Data.location + "`n"
+        $NumberMissingADKeys++
+    } else {
+        $TestOutput = $TestOutput +   "     AD Key OK." + "`n"
+    }
+
+    ####################
+    # See if it has keys stored in the MBAM database
+    ####################
+    $SQLQuery = "SELECT LastUpdateTime FROM [MBAM Recovery and Hardware].[RecoveryAndHardwareCore].[Machines] WHERE Name like '$Name'"
+    $SqlConnection = New-Object System.Data.SqlClient.SqlConnection
+    $SqlConnection.ConnectionString = "Server = $SQLServer; Database = $SQLDBName; Integrated Security = True" 
+    $SqlCmd = New-Object System.Data.SqlClient.SqlCommand
+    $SqlCmd.CommandText = $SqlQuery
+    $SqlCmd.Connection = $SqlConnection 
+    $SqlAdapter = New-Object System.Data.SqlClient.SqlDataAdapter
+    $SqlAdapter.SelectCommand = $SqlCmd 
+    $DataSet = New-Object System.Data.DataSet
+    $SqlAdapter.Fill($DataSet) 
+    $SqlConnection.Close()
+ 
+    if (!$DataSet.Tables[0]) {
+        $TestOutput = $TestOutput +   "     ***No MBAM key found!***" + "`n"
+        $ComputersWithNoMBAMKey = $ComputersWithNoMBAMKey + $Name.name + " (" + $path + ")`n"
+        $NumberMissingMBAMKeys++
+    } else {
+        $TestOutput = $TestOutput +   "     MBAM key OK." + "`n"
+    }
+
+    $TestOutput = $TestOutput +   "`n"
+    clear
+
+}
+
+###########################
+# Verify keys are in MBAM if they are in AD
+###########################
+$keys = Get-ADObject -LDAPFilter "(objectClass=msFVE-RecoveryInformation)" -SearchBase $path -SearchScope Subtree
+$ADToMBMMissingNum = 0
+$ADtoMBAMCheckOutput = ""
+$ADtoMBAMCheckOutput = $ADtoMBAMCheckOutput + "`n`n"
+$ADtoMBAMCheckOutput = $ADtoMBAMCheckOutput + "==================================`n"
+$ADtoMBAMCheckOutput = $ADtoMBAMCheckOutput + "Keys In AD, But Missing From MBAM: " + $ADToMBMMissingNum + "`n"
+$ADtoMBAMCheckOutput = $ADtoMBAMCheckOutput + "==================================`n"
+$ADtoMBAMCheckOutput = $ADtoMBAMCheckOutput + "`n`n"
+
+foreach ($item in $keys) {
+    $data = $item.ToString()
+    $data = $data.Substring(29,36)
+    $data = $data.ToLower()
+
+
+    ####################
+    # See if it has keys stored in the MBAM database
+    ####################
+    $SQLServer = "mbamdb.ad.emich.edu"
+    $SQLDBName = "MBAM Recovery and Hardware"
+    $SQLQuery = "SELECT * FROM [MBAM Recovery and Hardware].[RecoveryAndHardwareCore].[Keys] WHERE RecoveryKeyId = '`$data'"
+    $SqlConnection = New-Object System.Data.SqlClient.SqlConnection
+    $SqlConnection.ConnectionString = "Server = $SQLServer; Database = $SQLDBName; Integrated Security = True" 
+    $SqlCmd = New-Object System.Data.SqlClient.SqlCommand
+    $SqlCmd.CommandText = $SqlQuery
+    $SqlCmd.Connection = $SqlConnection 
+    $SqlAdapter = New-Object System.Data.SqlClient.SqlDataAdapter
+    $SqlAdapter.SelectCommand = $SqlCmd 
+    $DataSet = New-Object System.Data.DataSet
+    $SqlAdapter.Fill($DataSet) 
+    $SqlConnection.Close()
+ 
+    if (!$DataSet.Tables[0]) {
+       $ADtoMBAMCheckOutput = $ADtoMBAMCheckOutput + $data + " not found in MBAM, but is in AD!`n"
+       $ADToMBMMissingNum++
+    } 
+
+    clear
+}
+
+# Compile output from AD test
+$ADOutput = ""
+$ADOutput = $ADOutput + "`n`n"
+$ADOutput = $ADOutput + "==================================`n"
+$ADOutput = $ADOutput + "Computers Missing Keys In AD: " + $NumberMissingADKeys + "`n"
+$ADOutput = $ADOutput + "==================================`n"
+$ADOutput = $ADOutput + $ComputersWithNoADKey
+$ADOutput = $ADOutput + "`n`n"
+
+# Compile output from MBAM test
+$MBAMOutput = ""
+$MBAMOutput = $MBAMOutput + "==================================`n"
+$MBAMOutput = $MBAMOutput + "Computers Missing Keys In MBAM: " + $NumberMissingMBAMKeys + "`n"
+$MBAMOutput = $MBAMOutput + "==================================`n"
+$MBAMOutput = $MBAMOutput + $ComputersWithNoMBAMKey
+$MBAMOutput = $MBAMOutput + "`n`n"
+
+# Compile final output
+$FinalOut = $ADOutput + $MBAMOutput + $ADtoMBAMCheckOutput + $TestOutput
+
+# Post final output
+write-host $FinalOut
---
+++ run-remote-desktop-from-google-spreadsheet.ps1
@@ -0,0 +1,109 @@
+﻿########################
+# Written by: Ben Peters
+# 5/19/2017
+#
+# This tool will read a google spreadsheet, for any lines for which you are marked as a task admin.
+# It will check the status line, and if it's set to your desired status, it will try to open an RDP session if you
+# do not already have it open.  Cool.
+#
+# Instructions:
+# 1 - Copy https://github.com/prasmussen/gdrive to somewhere on your machine (it assumes somewhere on C drive).
+# 2 - Rename the executable you downloaded to just 'gdrive.exe'
+# 3 - Open up a command prompt, and run the gdrive program.  It should walk you through OAUTH authorization.
+# 4 - Edit this script, and change the relevant paths / file names in the "Variable Configuration" section.
+#
+#########################
+
+###############
+# Variable Configuration
+###############
+# How long should I wait between checks (In seconds)?
+$Delay = 30
+
+# Who are you? I.E. who should it look for to know the ask is assigned to you?
+$user = 'bpeters'
+
+# What's the name of the Google sheet you are working with?
+$SheetName = '<name of sheet>'
+
+# What is the header for the column containing the name of the person responsible?
+$AdminNameHeader = 'Task Admin'
+
+# What is the header for the column containing the hostnames?
+$HostNameHeader = 'Host'
+
+# What is the header for the column containing the status, and what status should I look for?
+$StatusHeader = 'Host Status'
+$StatusToUse = 'patching'
+
+# Where is the gdrive program located?
+$InputFilePath = "C:\Users\<username>\Downloads\"
+
+# Debugging?
+$ShowDebugMessages = "yes"
+
+###################
+# Script stuff
+###################
+
+# Get the google drive file name, then download the most recent copy
+$Command = $InputFilePath + "gdrive.exe list --no-header --query `"name contains '$SheetName'`""
+$GCommand = Invoke-Expression $Command
+$GoogleFile = $GCommand -split " "
+$GoogleFile = $GoogleFile -split " "
+$GoogleFile = $GoogleFile[0]
+$InputFile = $InputFilePath + $SheetName 
+
+# Loop this baby until we're done!
+while ($true) {
+
+    clear
+    
+    if ($ShowDebugMessages -eq "yes") {
+        write-host "----------start debug----------"
+        write-host "Path to gdrive.exe executable: " $InputFilePath
+        write-host "Name of Google worksheet: " $SheetName
+        write-host "What we are searching on in sheet: " $AdminNameHeader "=" $user ", " $StatusHeader "=" $StatusToUse
+        write-host "Delay between runs (seconds): " $Delay
+        write-host "----------end debug----------"
+        write-host ""
+    }
+
+    # Download and Import the working file to see status
+    write-host $(Get-Date -Format g) "Downloading current spreadsheet..."
+    write-host "-----------------------------------"
+    $Command = $InputFilePath + "gdrive.exe export $GoogleFile --force"
+    Invoke-Expression $Command
+    $ServersBeingPatched = Import-Csv $InputFile -EA Ignore | Where-Object {$_.$StatusHeader -eq $StatusToUse -and $_.$AdminNameHeader -eq $user}
+
+    # Get all instances of the remote desktop app that are running
+    $RunningRemoteDesktop = Get-Process mstsc -EA SilentlyContinue | where {$_.mainWindowTitle}
+
+    # Find out which of the ones listed as patching doesn't have an active RDP Session
+    foreach ($server in $ServersBeingPatched) {
+        $connected = "no"
+        foreach ($session in $RunningRemoteDesktop) {
+            if ($session.MainWindowTitle.ToLower() -match $server.Host.ToLower()) {
+                $connected = "yes"
+            }
+        }
+
+        # If we're not connected to an active session, try to do so, if it's actually online
+        if ($connected -eq "no") {
+            if (test-connection -Count 1 $server.Host) {
+                write-host "    " $server.Host " is online!  Attempting RDP session..."
+                mstsc /v: $server.Host
+            } else {
+                write-host "    "  $server.Host " is offline... will try reconnecting at next pass."
+            }
+        } else {
+            write-host "    "  $server.Host " is currently connected."
+        }
+
+    }
+
+    write-host "---------------------------------------------"
+    write-host "$(Get-Date -Format g) Sleeping" $Delay "seconds..."
+    Start-Sleep $Delay
+
+}
